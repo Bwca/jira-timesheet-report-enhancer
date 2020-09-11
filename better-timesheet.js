d8830f4d9483e42ce62cb22899ff13fe02d76a4c
Volodymyr <i.m.bwca@gmail.com>
2020-09-11 21:12:38 +0300

    working draft

--- better-timesheet.js
+++ better-timesheet.js
@@ -1,8 +1,66 @@
-
-
-
 const thead = document.querySelectorAll('.aui thead').item(1);
 const tbody = document.querySelectorAll('.aui tbody').item(1);
+const theadHeaderCells = Array.from(thead.querySelectorAll('th[title]'));
 
-console.log(thead);
-console.log(tbody);
+theadHeaderCells.forEach(setClickableTh);
+
+const defaultColor = '#7a869a';
+const activeColor = 'rgb(221, 0, 0)';
+
+function setClickableTh(el, index) {
+
+    el.style.cursor = 'pointer';
+    el.onclick = () => {
+
+        if (el.style.color === activeColor) {
+            el.style.color = defaultColor;
+            unsort();
+            return;
+        }
+
+        theadHeaderCells.forEach(i => {
+            i.style.color = defaultColor;
+        });
+        el.style.color = activeColor;
+
+        sortByDate(index);
+    }
+}
+
+function unsort() {
+    const rows = getBodyRows();
+    rows.forEach(i => i.style.opacity = '1')
+}
+
+function sortByDate(thIndex) {
+    const rows = getBodyRows();
+    rows.pop();
+    const valueRowIndex = getCorrespondingBodyCell(thIndex);
+    console.log(rows);
+
+    const headRows = [];
+    const tailRows = [];
+
+    for (let row of rows) {
+        const cellForCheck = row.querySelectorAll('td').item(valueRowIndex);
+        const textContent = cellForCheck.textContent.trim();
+        if (textContent) {
+            row.style.opacity = '1';
+            headRows.push(row);
+        } else {
+            row.style.opacity = '.2';
+            tailRows.push(row);
+        }
+        tbody.removeChild(row);
+    }
+
+    tbody.prepend(...headRows, ...tailRows);
+}
+
+function getBodyRows() {
+    return Array.from(tbody.querySelectorAll('tr'));
+}
+
+function getCorrespondingBodyCell(thIndex) {
+    return thIndex + 5;
+}
